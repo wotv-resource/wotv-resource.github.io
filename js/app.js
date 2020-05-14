6394c78d5818a219e8cd02d1a25e5fa06228ebd2
Michael Hernandez <gammabeta010@gmail.com>
2020-05-14 14:37:15 +0200

    New CSS, JS and data files

---
+++ js/app.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+
+    var url = 'data/Units.json';
+
+    $.ajax({
+
+        url: url,
+        method: 'GET',
+        success: function(data) {
+            console.log(data);
+        },
+        error: function(error) {
+
+        }
+
+
+
+    });
+
+/*    readJSON('data/Units.json')
+
+function readJSON(path) {
+var xhr = new XMLHttpRequest();
+xhr.open('GET', path, true);
+xhr.responseType = 'blob';
+xhr.onload = function(e) {
+  if (this.status == 200) {
+      var file = new File([this.response], 'temp');
+      var fileReader = new FileReader();
+      fileReader.addEventListener('load', function(){
+           //do stuff with fileReader.result
+           console.log(fileReader.result);
+      });
+      fileReader.readAsText(file);
+  }
+}
+xhr.send();
+}
+
+*/
+/*
+fetch(url, {mode: 'no-cors'}) // Call the fetch function passing the url of the API as a parameter
+.then(function(data) {
+    console.log(data.response);
+})
+.catch(function() {
+// This is where you run code if the server returns any errors
+});
+*/
+})
